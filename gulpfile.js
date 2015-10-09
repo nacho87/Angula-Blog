921d78c9e826beff652816979fb638b5822c2fe0
Nacho Decima <jidecima87@gmail.com>
2015-10-08 21:02:55 -0300

    Add configuration for gulp.task css,html, server and watch.

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,4 @@
+//Require for Gulp and Server
 var gulp = require('gulp'),
 	connect = require('gulp-connect'),
 	historyApiFallback = require('connect-history-api-fallback');
@@ -10,7 +11,40 @@ gulp.task('server', function(){
 		port: 8080,
 		livereload: true,
 		middleware: function(connect, opt){
-			return [ historyApiFallback];
+			return [ historyApiFallback()];
 		}
 	});
 });
+
+
+//Stylus
+
+var stylus = require('gulp-stylus');
+	nib = require('nib');
+
+
+//Prepros the files .styl to css and reload the page.
+gulp.task('css', function(){
+	gulp.src('./app/stylesheets/main.styl')
+		.pipe(stylus({ use: nib() }))
+		.pipe(gulp.dest('./app/stylesheets'))
+		.pipe(connect.reload());
+});
+
+
+//Reload the page if have changes in the HTML
+gulp.task('html', function(){
+	gulp.src('./app/**/*.html')
+	.pipe(connect.reload());
+});
+
+
+//Watch if have changes in HTML and CSS
+gulp.task('watch', function(){
+	gulp.watch(['./app/**/*.html'], ['html']);
+	gulp.watch(['./app/stylesheets/**/*.styl'], ['css']);
+});
+
+
+//Default Task
+gulp.task('default', ['server', 'watch']);
