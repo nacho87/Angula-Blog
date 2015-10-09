49afccbc5e553a72b4a8a9c18da5969e5577b60b
Nacho Decima <jidecima87@gmail.com>
2015-10-09 18:49:14 -0300

    Add Html5 option and beautify the urls.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -2,5 +2,11 @@
 'use strict';
 	//Here the funcionality
 
-	angular.module('blog',['ngRoute'])
+	angular.module('blog',['ngRoute']);
+
+	function config($locationProvider, $routeProvider){
+		$locationProvider.html5Mode('true');
+
+	}
+
 })();
