8152237c4d826018ada163fd4bd09678c74b6a3c
Nacho Decima <jidecima87@gmail.com>
2015-10-10 01:38:03 -0300

    Add controller and sevices to angular app.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -2,11 +2,31 @@
 'use strict';
 	//Here the funcionality
 
-	angular.module('blog',['ngRoute']);
+	angular.module('blog',['ngRoute', 'blog.controllers']);
 
 	function config($locationProvider, $routeProvider){
-		$locationProvider.html5Mode('true');
+		$locationProvider.html5Mode(true);
 
+		$routeProvider
+			.when('/', {
+				templateUrl: 'views/post-list.tpl.html',
+				controller: 'PostListController',
+				controllerAs: 'postlist'
+			})
+			.when('/post/:postId', {
+				templateUrl: 'views/post-details.tpl.html',
+				controller: 'PostDetailController',
+				controllerAs: 'postdetail'
+			})
+			.when('/new', {
+				templateUrl: 'views/post-create.tpl.html',
+				controller: 'PostCreateController',
+				controllerAs: 'postcreate'
+			});
 	}
+
+	angular
+		.module('blog')
+		.config(config);
 
 })();
