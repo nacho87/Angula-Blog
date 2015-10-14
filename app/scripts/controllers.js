d933ccbe76db642bc6192ba64a61e0c9988a6b52
Nacho Decima <jidecima87@gmail.com>
2015-10-14 00:16:06 -0300

    Add Google Maps to User View.

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -2,7 +2,7 @@
 	'use strict';
 
 	angular
-		.module('blog.controllers', ['blog.services'])
+		.module('blog.controllers', ['blog.services', 'uiGmapgoogle-maps'])
 		.controller('PostListController', PostListController)
 		.controller('PostDetailController', PostDetailController)
 		.controller('PostCreateController', PostCreateController)
@@ -44,8 +44,43 @@
 	}
 
 	function UserDetailsController($routeParams, User){
-		this.user = User.query({id: $routeParams.userId});
-		console.log('test');
-		console.log	(this.user);
+		var self = this;// to save the ref
+		User.query({id: $routeParams.userId})
+		.$promise.then(
+			function (data){
+				self.user = data;
+				var glat = self.user[0].address.geo.lat;
+				var glng = self.user[0].address.geo.lng;
+
+				self.map = {
+					center : {
+						latitude: glat,
+						longitude: glng
+					},
+					zoom: 15
+				};
+				self.marker = {
+					id: 0,
+					coords:{
+						latitude: glat,
+						longitude: glng,
+					},
+					show: false,
+				};
+				self.windowOptions = {
+					visible: false
+				};
+
+				self.onClick = function() {
+					self.windowOptions.visible = !self.windowOptions.visible;
+				};
+
+				self.closeClick = function() {
+					self.windowOptions.visible = false;
+				};
+
+				self.title = "Save Me!!";
+			}
+		);
 	}
 })();
